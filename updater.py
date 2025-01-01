543fb04e7783ec95078d2cddbe76a510d49024cf
luceberia <fncp12@icloud.com>
2025-01-02 02:19:07 +0900

    Initial release v0.0.2

--- updater.py
+++ updater.py
@@ -3,14 +3,11 @@ from PyQt6.QtWidgets import QMessageBox, QDialog
 from config.system.log_config import setup_logging
 from ui.components.update_dialog import UpdateDialog
 from version import __version__
+from packaging.version import parse as parse_version
 import requests
 import webbrowser
 
 logger = setup_logging()
-
-# 시스템의 기본 인증서 사용
-os.environ['SSL_CERT_FILE'] = '/etc/ssl/cert.pem'
-os.environ['REQUESTS_CA_BUNDLE'] = '/etc/ssl/cert.pem'
 
 def check_for_updates(current_version, parent=None):
     try:
@@ -21,19 +18,24 @@ def check_for_updates(current_version, parent=None):
             latest_release = response.json()
             latest_version = latest_release['tag_name'].replace('v', '')
             
-            version_info = {
-                'current': current_version,
-                'latest': latest_version
-            }
-            
-            logger.debug(f"현재 버전: {current_version}, 최신 버전: {latest_version}")
-            dialog = UpdateDialog(parent, version_info, is_update=True)
-            result = dialog.exec()
-            
-            if result == QDialog.DialogCode.Accepted:
-                logger.info("사용자가 업데이트를 승인했습니다.")
-                webbrowser.open(latest_release['html_url'])
-            
+            # 버전 비교 로직 추가
+            if parse_version(latest_version) > parse_version(current_version):
+                version_info = {
+                    'current': current_version,
+                    'latest': latest_version
+                }
+                
+                logger.debug(f"현재 버전: {current_version}, 최신 버전: {latest_version}")
+                dialog = UpdateDialog(parent, version_info, is_update=True)
+                result = dialog.exec()
+                
+                if result == QDialog.DialogCode.Accepted:
+                    logger.info("사용자가 업데이트를 승인했습니다.")
+                    webbrowser.open(latest_release['html_url'])
+            else:
+                logger.info("최신 버전을 사용 중입니다.")
+                QMessageBox.information(parent, "업데이트 확인", "현재 최신 버전을 사용 중입니다.")
+                
     except Exception as e:
         logger.error(f"업데이트 확인 중 오류 발생: {e}")
         error_dialog = UpdateDialog(parent, {'current': str(e)}, is_update=False)
